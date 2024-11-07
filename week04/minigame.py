351df96cd9f3a24ea0f729355613eea08b1e3f75
rungsima-tnk <67319010015@tnk.ac.th>
2024-11-07 20:59:03 +0700

    week04

--- week04/minigame.py
+++ week04/minigame.py
@@ -1,21 +1,27 @@
 import random
-print("โปรเเกรมเป่ายิงฉุบ")
-w = 0
-f = 0
-while True:
-    b = random.choice(["ค้อน","กรรไกร","กระดาษ"])
-    print(b)
-    print("เลือก ค้อน กรรไกร กระดาษ ")
-    a = str(input("คุณเลือก :"))
-    if a == b :
-        print("เสมอ") 
-    elif a == "ค้อน" and b == "กรรไกร" :
+print('เกมเป่ายิงฉุบ')
+win = 0
+lose = 0
+always = 0
+for รอบ in range(1,5+1):
+    bot = random.choice(["ค้อน","กรรไกร","กระดาษ"])
+    print('-----------------------------')
+    print(bot)
+    print("เลือก ค้อน กรรไกร กระดาษ")
+    player = str(input("คุณเลือก :"))
+    if player == bot :
+        always += 1
+        print("เสมอ")
+    elif (player == "ค้อน" and bot == "กรรไกร"):
+        win += 1
         print("ชนะ")
-    elif a == "กรรไกร" and b == "กระดาษ" :
+    elif (player == "กระดาษ" and bot == "ค้อน"):
+        win += 1
         print("ชนะ")
-    elif a == "กระดาษ" and b == "ค้อน" :
+    elif (player == "กรรไกร" and bot == "กระดาษ"):
+        win += 1
         print("ชนะ")
-    else:
-        print("เเพ้")
-        f += 1
-print(f"")
+    else :
+        lose += 1
+        print("แพ้")
+print(f"เล่นเสร็จสิ้น!! ชนะทั้งหมด: {win} แพ้ทั้งหมด: {lose} เสมอทั้งหมด: {always}")
