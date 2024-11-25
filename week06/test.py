e13e7a662d013894c0dfe260f4f7ed117ee1b043
rungsima-tnk <67319010015@tnk.ac.th>
2024-11-25 15:33:42 +0700

    week07

---
+++ week06/test.py
@@ -0,0 +1,11 @@
+# num = int(input(" : "))
+# for i in range (1,num+1):
+#     if i % 2 != 0 :
+#         print(i)
+
+a = int(input("ใส่เลข"))
+x = 0
+while x < a:
+    x += 1
+    if x % 2 != 0:
+        print(x)
